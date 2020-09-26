39057fb801108d85b511e0c936dd5361e9c2bcdf
Haswell <shuyangf@student.unimelb.edu.au>
2020-09-27 08:11:52 +1000

    Change API base path to dev

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 var instance = axios.create({
-    // baseURL: "https://codebrew.haswf.com/api"
-    baseURL: "http://localhost:3000/api"
+    baseURL: "https://codebrew.haswf.com/api"
+    // baseURL: "http://localhost:3000/api"
 
 });
 
