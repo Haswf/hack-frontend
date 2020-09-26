6140d7126fcdf9ce4eff55d68ea07c4787b7f65d
gmy0516 <56905209+gmy0516@users.noreply.github.com>
2020-09-27 05:46:09 +1000

    filter (not complete)

--- src/component/List.js
+++ src/component/List.js
@@ -45,66 +45,13 @@ const ListDragItem = styled.div`
 
 class List extends Component {
   state = {
+    listTags:JSON.parse(sessionStorage.getItem("tag_filter")),
     discussions: [],
     listTitle: "Featured for members",
     listBreadcrumb: "Home / Articles",
     items:[]
-    /*
-    items: [
-      {
-        id: 0,
-        hasActions: false,
-        textValue: "I have a headache",
-        lastUser: "Lida Brady",
-        daysAgo: "5",
-        image:
-          "https://images.unsplash.com/photo-1542140372-de3e121eb11e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6feeb58669ba6adbd2aacc9c89391713&auto=format&fit=crop&w=200&q=80",
-        person1_image:
-          "https://images.unsplash.com/photo-1504703395950-b89145a5425b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d702cb99ca804bffcfa8820c46483264&auto=format&fit=crop&w=200&q=80",
-        person2_image:
-          "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=afb35d2683e102d67bcd70b87b100723&auto=format&fit=crop&w=200&q=80"
-      },
-      {
-        id: 1,
-        hasActions: true,
-        textValue: "I think I have Covid19",
-        lastUser: "Dan Abramov",
-        daysAgo: "2",
-        image:
-          "https://images.unsplash.com/photo-1542149624-8a12d5285934?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a91f847fdcc99b00a29d5a39a2b6f4b9&auto=format&fit=crop&w=200&q=80",
-        person1_image:
-          "https://images.unsplash.com/photo-1510665217504-ea639bd64d3b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=35eb579302184195d572e7f8caabaf23&auto=format&fit=crop&w=200&q=80",
-        person2_image:
-          "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=200&q=80"
-      },
-      {
-        id: 2,
-        hasActions: false,
-        textValue: "I am dying",
-        lastUser: "Flavio Copes",
-        daysAgo: "6",
-        image:
-          "https://images.unsplash.com/photo-1542274368-443d694d79aa?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=13f276041134f4982218e49b446bedb1&auto=format&fit=crop&w=200&q=80",
-        person1_image:
-          "https://images.unsplash.com/photo-1520810336800-28d1a1d41906?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f0f86b0656f7f07126b142faf38927c1&auto=format&fit=crop&w=200&q=60",
-        person2_image:
-          "https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=77b4ffa2acda61616908a62c5c1209af&auto=format&fit=crop&w=200&q=80"
-      },
-      {
-        id: 3,
-        hasActions: true,
-        textValue: "I got cancer, pleas help!",
-        lastUser: "Casper Beyer",
-        daysAgo: "2",
-        image:
-          "https://images.unsplash.com/photo-1542144612-1b3641ec3459?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c00cb83d290ac6e0b59ac95fcf46ee0e&auto=format&fit=crop&w=200&q=80",
-        person1_image:
-          "https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=77b4ffa2acda61616908a62c5c1209af&auto=format&fit=crop&w=200&q=80",
-        person2_image:
-          "https://images.unsplash.com/photo-1504703395950-b89145a5425b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d702cb99ca804bffcfa8820c46483264&auto=format&fit=crop&w=200&q=80"
-      }
-    ]*/
   };
+
 
   async componentDidMount() {
     const response = await axios.get("/discussions/", {});
@@ -124,6 +71,11 @@ class List extends Component {
 
 
     this.setState({items: items})
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(this.state);
+
   }
 
   reorderItems = (startIndex, endIndex) => {
