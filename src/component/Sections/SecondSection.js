53579bdd93efc341184c46b8cfd050fce9b8003b
Jiayi-Joyce-Lu <jllu1@student.unimelb.edu.au>
2020-09-27 07:05:30 +0800

    home

--- src/component/Sections/SecondSection.js
+++ src/component/Sections/SecondSection.js
@@ -19,9 +19,10 @@ export default function WorkSection() {
         <div className={classes.section}>
             <GridContainer justify="center">
                 <GridItem cs={12} sm={12} md={8}>
-                    <h2 className={classes.title}>Who are we?</h2>
+                    <h2 className={classes.title}>Protect yourself and others from COVID-19</h2>
                     <h4 className={classes.description}>
-                        We are ......
+                        Maintain at least 1 metre (3 feet) distance between yourself and others. Why? When someone coughs, sneezes, or speaks they spray small liquid droplets from their nose or mouth which may contain virus. If you are too close, you can breathe in the droplets, including the COVID-19 virus if the person has the disease.
+                        Avoid going to crowded places. Why? Where people come together in crowds, you are more likely to come into close contact with someone that has COVID-19 and it is more difficult to maintain physical distance of 1 metre (3 feet).
                     </h4>
                     <form>
                         <GridContainer>
--- src/component/Sections/WorkSection.js
+++ src/component/Sections/WorkSection.js
@@ -21,7 +21,11 @@ export default function WorkSection() {
         <GridItem cs={12} sm={12} md={8}>
           <h2 className={classes.title}>The Greatest Threat for Humans in Decades</h2>
           <h4 className={classes.description}>
-            The world now ......
+            Coronaviruses are a large family of viruses that are known to cause illness ranging from the common cold to more severe diseases such as Middle East Respiratory Syndrome (MERS) and Severe Acute Respiratory Syndrome (SARS).
+
+            A novel coronavirus (COVID-19) was identified in 2019 in Wuhan, China. This is a new coronavirus that has not been previously identified in humans.
+
+            The coronavirus COVID-19 pandemic is the defining global health crisis of our time and the greatest challenge we have faced since World War Two. Since its emergence in Asia late last year, the virus has spread to every continent except Antarctica.
           </h4>
           <form>
             <GridContainer>
