41f21a5feef85d808694f10614638f26d2dba448
Jiayi-Joyce-Lu <jllu1@student.unimelb.edu.au>
2020-09-27 03:44:34 +0800

    homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,59 @@
 import React from "react";
-import PrimarySearchAppBar from "../component/AppBar";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
 
+// @material-ui/icons
 
-export default function () {
-    return(
-        <div></div>
+// core components
+
+import Footer from "./Footer/Footer.js";
+import GridContainer from "./Grid/GridContainer.js";
+import GridItem from "./Grid/GridItem.js";
+
+import Parallax from "./Parallax/Parallax.js";
+
+import styles from "./LandingPage/landingPage";
+
+// Sections for this page
+
+import WorkSection from "../component/Sections/WorkSection";
+
+const dashboardRoutes = [];
+
+const useStyles = makeStyles(styles);
+
+export default function LandingPage(props) {
+    const classes = useStyles();
+    const { ...rest } = props;
+    return (
+        <div>
+
+            <Parallax filter image={require("./LandingPage/landing-bg.jpg")}>
+                <div className={classes.container}>
+                    <GridContainer>
+                        <GridItem xs={12} sm={12} md={6}>
+                            <h1 className={classes.title}>Your Story Starts With Us.</h1>
+                            <h4>
+                                Every landing page needs a small description after the big bold
+                                title, that{"'"}s why we added this text here. Add here all the
+                                information that can make you or your product create the first
+                                impression.
+                            </h4>
+                            <br />
+
+                        </GridItem>
+                    </GridContainer>
+                </div>
+            </Parallax>
+            <div className={classNames(classes.main, classes.mainRaised)}>
+                <div className={classes.container}>
+
+                    <WorkSection />
+                </div>
+            </div>
+            <Footer />
+        </div>
     );
 }
